3f2097034547295dc4a99965075207c9ff3634ba
Furyn <vincent_1999@hotmail.fr>
2022-04-06 11:05:28 +0200

    [ADD] Prefab des objets important + design du sol

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -5,8 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+
+    public GameObject level_panel = null;
+    public GameObject main_panel = null;
+
     public void PlayButton()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void OpenLevelPanel()
+    {
+        level_panel.SetActive(true);
+        main_panel.SetActive(false);
+    }
+
+    public void OpenLevel(int LevelInt)
+    {
+        SceneManager.LoadScene(LevelInt);
     }
 }
